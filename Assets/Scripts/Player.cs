1c5c2e596ab44f46b6bec16f635f0bd47579db18
Mikael <mika.fr99hotmail.com>
2020-10-23 22:57:41 +0200

    PlayerController

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] private float speed = 10.0f;
+    private Rigidbody2D rb2d;
+    private Vector2 moveVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f);
+        transform.position = transform.position + moveInput * Time.deltaTime * speed;
+    }
+
+    void FixedUpdate()
+    {
+        //rb2d.MovePosition(rb2d.position + moveVelocity * Time.fixedDeltaTime);
     }
 }

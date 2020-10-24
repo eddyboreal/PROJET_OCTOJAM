ee074f56ce8e1945c157fc429ebbf13aedebcaf5
Eddy <eddy.abada.ea@gmail.com>
2020-10-24 12:33:31 +0200

    shot generation and spawning logic done

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -7,28 +7,36 @@ public class SceneManager : MonoBehaviour
 {
     // list of players spawned in the scene
     private GameObject[] players;
+    // list of locations in the scene
+    private GameObject[] locations;
     // number of shots in one player sequence
     private int sequences_length = 6;
     // number of shots added arbitrary in the scene
     [SerializeField]
     private int nb_additional_shots = 6;
+    // list of shot objects spawned in the scene
+    [SerializeField]
+    private List<GameObject> _shots = new List<GameObject>();
 
     // all the shots in the scene
     private Dictionary<string, int> shots = new Dictionary<string, int>
     {
         {"El Verdito", 0 },
-        {"El Rojito", 0 },
+        {"El Naranjo", 0 },
         {"La Rosita", 0 },
         {"La Negrita", 0 },
         {"El Bermillon", 0 },
-        {"El Blancito", 0 }
+        {"El Grisito", 0 }
     };
 
     void Start()
     {
         players = GameObject.FindGameObjectsWithTag("Player");
+        locations = GameObject.FindGameObjectsWithTag("Location");
         generateSequences();
         generateAdditionalShots();
+        createShots();
+        locateShots();
     }
 
     /*void Update()
@@ -65,6 +73,34 @@ public class SceneManager : MonoBehaviour
             int rd_shot = Random.Range(0, shots.Count);
             string shot_key = shots.ElementAt(rd_shot).Key;
             if (shots.ContainsKey(shot_key)) shots[shot_key] += 1;
+        }
+    }
+
+    // creating and spawning all the shots needed 
+    private void createShots()
+    {
+        foreach(KeyValuePair<string, int> kvp in shots)
+        {
+            for (int i = 0; i < kvp.Value; ++i)
+            {
+                GameObject shot = Instantiate(Resources.Load("Shot")) as GameObject;
+                shot.GetComponent<Shot>().SetLabel(kvp.Key);
+                shot.GetComponent<Shot>().ApplyColor();
+                _shots.Add(shot); 
+            }
+        }
+    }
+
+    // locate all shots
+    private void locateShots()
+    {
+        List<GameObject> _locationsL = new List<GameObject>(locations);
+        foreach (GameObject s in _shots)
+        {
+            int index = Random.Range(0, _locationsL.Count);
+            s.transform.parent = _locationsL[index].transform;
+            s.transform.localPosition = new Vector3(0, 0, -0.3f);
+            _locationsL.RemoveAt(index);
         }
     }
 }
--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Shot : MonoBehaviour
 {
+    public string label;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,40 @@ public class Shot : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SetLabel(string label)
+    {
+        this.label = label;
+    }
+
+    // apply color to shot
+    public void ApplyColor()
+    {
+        switch (label)
+        {
+            case "El Verdito":
+                this.GetComponent<SpriteRenderer>().color = Color.green;
+                break;
+            case "El Naranjo":
+                this.GetComponent<SpriteRenderer>().color = new Color(0.96f, 0.54f, 0f);
+                break;
+            case "La Rosita":
+                this.GetComponent<SpriteRenderer>().color = new Color(0.96f, 0.22f, 0.95f);
+                break;
+            case "La Negrita":
+                this.GetComponent<SpriteRenderer>().color = Color.black;
+                break;
+            case "El Bermillon":
+                this.GetComponent<SpriteRenderer>().color = new Color(0.70f, 0.22f, 0.07f);
+                break;
+            case "El Grisito":
+                this.GetComponent<SpriteRenderer>().color = Color.gray;
+                break;
+            default:
+                Color c = Color.white;
+                break;
+        }
     }
 
 }

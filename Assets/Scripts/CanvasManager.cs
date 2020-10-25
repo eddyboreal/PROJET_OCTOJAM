330aeab5c0ea7412e248c2d42dabcef771392172
Mikael <mika.fr99hotmail.com>
2020-10-25 13:11:42 +0100

    MainMenu

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasManager : MonoBehaviour
+{
+    public Image MainMenuImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMainMenuClick()
+    {
+        MainMenuImage.GetComponent<Animator>().SetBool("clicked", true);
+    }
+
+    public void NextScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerInputUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerInputUI : MonoBehaviour
+{
+
+    private InputAction buttonAction2;
+    PlayerInput playerInput;
+    public GameObject CanvasManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+        buttonAction2 = new InputAction();
+        buttonAction2 = playerInput.actions.ElementAt(2);
+        buttonAction2.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (buttonAction2.triggered)
+        {
+            OnMenuClick();
+        }
+    }
+
+
+    public void OnMenuClick()
+    {
+        Debug.Log("chiasse");
+        CanvasManager.GetComponent<CanvasManager>().OnMainMenuClick();
+    }
+}
